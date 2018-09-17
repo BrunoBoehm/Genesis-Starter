e674438ff1c9c39a701e754fc48c05d0546a74e6
BrunoBoehm <bruno.boehm@gmail.com>
2018-09-17 18:58:35 +0200

    Refactor ly_enqueue_scripts_styles to use dynamic $theme_text_domain

--- functions.php
+++ functions.php
@@ -73,14 +73,17 @@ function ly_theme_setup() {
 	// Adds the Genesis Connect WooCommerce notice.
 	require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.php';
 
+	// Enqueue
 	add_action( 'wp_enqueue_scripts', 'ly_enqueue_scripts_styles' );
 
 	// Sets the content width based on the theme's design and stylesheet.
+	// https://codex.wordpress.org/Content_Width
 	if ( ! isset( $content_width ) ) {
 		$content_width = 702; // Pixels.
 	}
 
-	// Adds support for HTML5 markup structure.
+	// Adds full support for HTML5 markup structure.
+	// https://codex.wordpress.org/Theme_Markup
 	add_theme_support(
 		'html5', array(
 			'caption',
@@ -179,7 +182,7 @@ function ly_theme_setup() {
 function ly_enqueue_scripts_styles() {
 
 	wp_enqueue_style(
-		'genesis-starter-fonts',
+		"{$theme_text_domain}-fonts",
 		'//fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,700',
 		array(),
 		CHILD_THEME_VERSION
@@ -188,7 +191,7 @@ function ly_enqueue_scripts_styles() {
 
 	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 	wp_enqueue_script(
-		'genesis-starter-responsive-menu',
+		"{$theme_text_domain}-responsive-menu",
 		get_stylesheet_directory_uri() . "/js/responsive-menus{$suffix}.js",
 		array( 'jquery' ),
 		CHILD_THEME_VERSION,
@@ -196,13 +199,13 @@ function ly_enqueue_scripts_styles() {
 	);
 
 	wp_localize_script(
-		'genesis-starter-responsive-menu',
+		"{$theme_text_domain}-responsive-menu",
 		'genesis_responsive_menu',
-		ly_responsive_menu_settings()
+		ly_responsive_menu_settings() // called below
 	);
 
 	wp_enqueue_script(
-		'genesis-starter',
+		"{$theme_text_domain}",
 		get_stylesheet_directory_uri() . '/js/base.js',
 		array( 'jquery' ),
 		CHILD_THEME_VERSION,
